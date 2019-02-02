dcd07575f40d02113ee7485895f8af1d2326dcbf
Natalia Ayupova <natalia.ayupova@gmail.com>
2019-02-02 17:47:21 +0100

    implemented challenges

--- Challenge1/main/problem1.go
+++ Challenge1/main/problem1.go
@@ -2,52 +2,52 @@ package main
 
 import (
 	"log"
-	"time"
 	"math/rand"
+	"sync"
 )
+
+/**
+Synchronized counter
+*/
+type Counter struct {
+	counter int
+	mutex   sync.Mutex
+}
 
 func problem1() {
 
 	log.Printf("problem1: started --------------------------------------------")
-
-	//
-	// Todo:
-	//
-	// Quit all go routines after
-	// a total of exactly 100 random
-	// numbers have been printed.
 	//
 	// Do not change the 25 in loop!
 	//
-
+	// wait group to wait till ll go routines are finished
+	waitGroup := sync.WaitGroup{}
+	counter := Counter{}
 	for inx := 0; inx < 10; inx++ {
-
-		go printRandom1(inx)
-
+		waitGroup.Add(1)
+		go printRandom1(inx, &counter, &waitGroup)
 	}
 
-	//
-	// Todo:
-	//
-	// Remove this quick and dirty sleep
-	// against a synchronized wait until all
-	// go routines are finished.
-	//
-
-	time.Sleep(5 * time.Second)
-
+	waitGroup.Wait()
 	log.Printf("problem1: finised --------------------------------------------")
 }
 
-func printRandom1(slot int) {
-
+func printRandom1(slot int, counter *Counter, waitGroup *sync.WaitGroup) {
+	defer waitGroup.Done()
 	//
 	// Do not change 25 into 10!
 	//
-
+	// lock the counter, check the value, if it's 100 already break and finish go routine
+	// otherwise increment counter, release lock
 	for inx := 0; inx < 25; inx++ {
+		counter.mutex.Lock()
+		if counter.counter == 100 {
+			counter.mutex.Unlock()
+			break
+		}
+		counter.counter++
+		counter.mutex.Unlock()
 
 		log.Printf("problem1: slot=%03d count=%05d rand=%f", slot, inx, rand.Float32())
-
 	}
 }
--- Challenge1/main/problem2.go
+++ Challenge1/main/problem2.go
@@ -2,48 +2,55 @@ package main
 
 import (
 	"log"
-	"time"
 	"math/rand"
+	"sync"
+	"time"
 )
+
+/**
+Synchronized ticker
+*/
+type SynchTicker struct {
+	ticker *time.Ticker
+	mutex  sync.Mutex
+}
 
 func problem2() {
 
 	log.Printf("problem2: started --------------------------------------------")
-
-	//
-	// Todo:
-	//
-	// Throttle all go subroutines in a way,
-	// that every one second one random number
-	// is printed.
-	//
+	// wait group to wait till ll go routines are finished
+	waitGroup := sync.WaitGroup{}
+	syncTicker := SynchTicker{ticker: time.NewTicker(time.Second)}
 
 	for inx := 0; inx < 10; inx++ {
-
-		go printRandom2(inx)
-
+		waitGroup.Add(1)
+		go printRandom2(inx, &waitGroup, &syncTicker)
 	}
 
-	//
-	// Todo:
-	//
-	// Remove this quick and dirty sleep
-	// against a synchronized wait until all
-	// go routines are finished.
-	//
-	// Same as problem1...
-	//
-
-	time.Sleep(5 * time.Second)
+	waitGroup.Wait()
 
 	log.Printf("problem2: finished -------------------------------------------")
 }
 
-func printRandom2(slot int) {
+func printRandom2(slot int, waitGroup *sync.WaitGroup, syncTicker *SynchTicker) {
+	defer waitGroup.Done()
 
 	for inx := 0; inx < 10; inx++ {
-
+		waitOnTicker(syncTicker)
 		log.Printf("problem2: slot=%03d count=%05d rand=%f", slot, inx, rand.Float32())
+	}
+}
 
+/**
+locks the ticker and waits till it ticks, after that releases the lock so that other go routines can proceed
+*/
+func waitOnTicker(syncTicker *SynchTicker) {
+	defer syncTicker.mutex.Unlock()
+	syncTicker.mutex.Lock()
+	for {
+		select {
+		case <-syncTicker.ticker.C:
+			return
+		}
 	}
 }
